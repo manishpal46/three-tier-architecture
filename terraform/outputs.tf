b9bdfd53051c9bd3ba61ab4b2110cd55422ec678
manish <manishpal46@gmail.com>
2025-07-16 04:52:18 +0000

    Fixed errors again

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,10 +14,6 @@ output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
 
-output "private_ip" {
-  value = aws_instance.this.private_ip
-}
-
-output "public_ip" {
-  value = var.associate_public_ip ? aws_instance.this.public_ip : ""
+output "frontend_public_ip" {
+  value = aws_instance.frontend.private_ip
 }
